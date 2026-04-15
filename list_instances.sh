1bed496f4fd12c2d528813e34d45b1adb6e145c5
openfde <openfde@openfde.com>
2026-04-15 11:35:56 +0800

    support listing download ecs

--- list_instances.sh
+++ list_instances.sh
@@ -2,7 +2,15 @@
 
 if [ "$1" = "aosp" ];then
 	aliyun ecs DescribeInstances --InstanceName openfde_aosp_make --RegionId us-east-1
-else
+elif  [ "$1" = "deb" ];then
 	aliyun ecs DescribeInstances --InstanceName openfde_deb_make --RegionId cn-hangzhou
+elif  [ "$1" = "download" ];then
+	aliyun ecs DescribeInstances --InstanceName openfde_aosp_download --RegionId us-east-1
+elif  [ "$1" = "disk" ];then
+	if [ "$2" = "using" ];then
+		aliyun ecs DescribeDisks --RegionId us-east-1  --Status In_use
+	else
+		aliyun ecs DescribeDisks --RegionId us-east-1  --Status Available
+	fi
 fi
 
