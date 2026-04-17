97c4e2c435f3faf45d693d7b5e55770e10bdeb78
openfde <openfde@openfde.com>
2026-04-17 14:28:48 +0800

    set logpath

--- v2/delete_instance.sh
+++ v2/delete_instance.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+LOGPATH=/root/logs/delete_instances.log
 function log(){
   s=`date "+%y%m%d_%H%M%S:"`
-  echo  "$s $1" |tee -a /root/logs/delete_instances.log
+  echo  "$s $1" |tee -a $LOGPATH
 }
 
 log $@
@@ -17,7 +18,7 @@ if [ -z "$2" ];then
 fi
 sleep 10
 if [ "$1" = "deb" ];then
-	aliyun configure switch --profile hangzhou >> logs/delete_instances.log
+	aliyun configure switch --profile hangzhou >> $LOGPATH
 	id=`aliyun ecs DescribeInstances --InstanceIds "[\"$2\"]" --InstanceName openfde_deb_make |jq -r .Instances.Instance[0].InstanceId`
 	if [ "$id" != "$2" ];then
 		log "Error: id not match $2 and $id wont delete "
@@ -25,14 +26,14 @@ if [ "$1" = "deb" ];then
 	fi
 
 elif [ "$1" = "aosp" ];then
-	aliyun configure switch --profile default >> logs/delete_instances.log
+	aliyun configure switch --profile default >> $LOGPATH
 	id=`aliyun ecs DescribeInstances --InstanceIds "[\"$2\"]" --InstanceName openfde_aosp_make |jq -r .Instances.Instance[0].InstanceId`
 	if [ "$id" != "$2" ];then
 		log "Error: id not match $2 and $id wont delete "
 		exit 1
 	fi
 elif [ "$1" = "download" ];then
-	aliyun configure switch --profile default >> logs/delete_instances.log
+	aliyun configure switch --profile default >> $LOGPATH
 	id=`aliyun ecs DescribeInstances --InstanceIds "[\"$2\"]" --InstanceName openfde_aosp_download |jq -r .Instances.Instance[0].InstanceId`
 	if [ "$id" != "$2" ];then
 		log "Error: id not match $2 and $id wont delete "
@@ -43,15 +44,15 @@ else
 		log "Error: type $1 is not support"
 		exit 1
 	fi
-	aliyun ecs DeleteDisk --DiskId $2  --RegionId us-east-1 >> logs/delete_disk.log
+	aliyun ecs DeleteDisk --DiskId $2  --RegionId us-east-1 >> $LOGPATH
 	exit 0
 fi	
 log "delete instance $2"
-aliyun ecs DeleteInstance --InstanceId $2 --Force true >> logs/delete_instances.log
+aliyun ecs DeleteInstance --InstanceId $2 --Force true >> $LOGPATH
 if [ $? != 0 ];then
 	for i in {1..10}; do
 		log "delete instance again $2"
-		aliyun ecs DeleteInstance --InstanceId $2 --Force true >> logs/delete_instances.log
+		aliyun ecs DeleteInstance --InstanceId $2 --Force true >> $LOGPATH
 		if [ $? = 0 ];then
 			break
 		fi
