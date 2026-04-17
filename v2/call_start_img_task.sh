cdfc48d1dfb54f734da647583543081215b9285f
openfde <openfde@openfde.com>
2026-04-17 15:17:44 +0800

    delete disk if 2start_aosp_build failed cause on creating failed and so on

--- v2/call_start_img_task.sh
+++ v2/call_start_img_task.sh
@@ -2,6 +2,15 @@
 
 #call next in daemon mode
 s=`date "+%y%m%d_%H%M%S:"`
-echo "$s $@" >> /root/logs/call_start_make_img_task.log
-./2start_aosp_build.sh $@ &
+disk_id=$6
+LOGPATH=/root/logs/call_start_make_img_task.log
+echo "$s $@" >> $LOGPATH
+/root/v2/2start_aosp_build.sh $@ &
+callpid=$!
+wait $callpid
+ret=$?
+if [ $ret != 0 ];then
+	echo "2start_aosp_build.sh failed need to delete disk" >> $LOGPATH
+	/root/v2/delete_instance.sh disk $disk_id >> $LOGPATH
+fi
 exit 0
