0570eb6d926277a73ef4c009d9552c113db7b21f
openfde <openfde@openfde.com>
2026-09-17 10:12:20 +0800

    change the instancetype to a new one

--- v2/2start_aosp_build.sh
+++ v2/2start_aosp_build.sh
@@ -44,7 +44,6 @@ function w_log()
 
 w_log "$@"
 aliyun configure switch --profile default 1>/dev/null 2>&1
-
 disk_id=`aliyun ecs DescribeDisks --RegionId us-east-1 --DiskName aosp_source_disk_350G_$mode |jq -r .Disks.Disk[0].DiskId`
 if [[ -z "$disk_id" || "$disk_id" == "null" ]]; then
 	w_log "failed: disk id not found"
@@ -53,11 +52,16 @@ if [[ -z "$disk_id" || "$disk_id" == "null" ]]; then
 	#aliyun ecs DeleteInstance --InstanceId $instance_id --Force true --RegionId us-east-1
 	exit 1
 fi
+#instance_type="ecs.c9ae.8xlarge"
+#query whether it have stock for specific instance type
+#aliyun ecs DescribeAvailableResource --RegionId us-east-1 --ZoneId  us-east-1a  --DestinationResource InstanceType --SpotStrategy SpotAsPriceGo --InstanceType $insatnce_type
+instance_type="ecs.c5.8xlarge"
+
 w_log "step 1: create ecs instance"
 password=`grep password key.txt |awk -F " " '{print $2}' `
 #instance_id="i-0xi16uf7r4syux4z894e"
 if [ -z "$instance_id" ];then
-	instance_id=`aliyun ecs RunInstances --SpotStrategy SpotAsPriceGo --RegionId us-east-1 --InstanceType ecs.c9ae.8xlarge --ImageId ubuntu_22_04_x64_20G_alibase_20230907.vhd \
+	instance_id=`aliyun ecs RunInstances --SpotStrategy SpotAsPriceGo --RegionId us-east-1 --InstanceType $instance_type --ImageId ubuntu_22_04_x64_20G_alibase_20230907.vhd \
 	--SecurityGroupId sg-0xi22iuffog8ylfh0or5  --VSwitchId vsw-0xi3vuydn6xui661xeiwu  --SystemDisk.Category cloud_essd --SystemDisk.PerformanceLevel PL1 \
        	--Password $password \
 	--InternetChargeType PayByTraffic --InternetMaxBandwidthOut 100 --Amount 1 --InstanceName openfde_aosp_make |jq -r .InstanceIdSets.InstanceIdSet[0] `
